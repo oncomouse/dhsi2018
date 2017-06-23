fb37b6b82f953dfb9844440ebdf08d0946041707
oncomouse <oncomouse@gmail.com>
2017-06-23 15:26:14 -0500

    Better documentation

--- day01/json.js
+++ day01/json.js
@@ -1,17 +1,26 @@
+// The first two lines are how you read from a file in Node. All you need to know is that fileContents contains the JSON string stored in cv.json:
 var fs = require('fs');
+fs.readFile(__dirname + '/cv.json', function(err,fileContents) {
 
-fs.readFile(__dirname + '/cv.json', function(err,data){
-	data = JSON.parse(data);
+	// Convert the JSON string into program data:
+	var data = JSON.parse(fileContents);
+	
+	// Access the data:
 	console.log('My name is ' + data.name + '.');
 	console.log(' I currently work at ' + data.currentEmployment.institution + ' as a ' + data.currentEmployment.title + ' and have since ' + data.currentEmployment.startYear + '.');
 	console.log('I have had ' + data.previousEmployment.length + ' previous academic jobs.');
 	
+	// Change the data:
 	data.previousEmployment.push({
 		institution: 'Pennsylvania State University',
 		title: 'Graduate Student',
 		startYear: 2005,
 		endYear: 2011
 	});
-	data = JSON.stringify(data,null,2); // ,null,2 causes JSON to format in a human-readable way (that takes up more space and shouldn't be used unless people are actually reading this JSON).
-	fs.writeFile(__dirname + '/cv.json', data, function(err,data) {});
+	
+	// Serialize the updated data to a JSON string again:
+	fileContents = JSON.stringify(data,null,2); // ,null,2 causes JSON to format in a human-readable way (that takes up more space and shouldn't be used unless people are actually reading this JSON).
+	
+	// Write it back to the disk using Node:
+	fs.writeFile(__dirname + '/cv.json', fileContents, function(err,data) {});
 })
