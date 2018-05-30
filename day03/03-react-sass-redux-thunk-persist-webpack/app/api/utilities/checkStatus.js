a84818a9feb963d894c0edd5acc3a1e18ea1059f
oncomouse <oncomouse@gmail.com>
2018-05-30 16:31:31 -0500

    Updated request

---
+++ day03/03-react-sass-redux-thunk-persist-webpack/app/api/utilities/checkStatus.js
@@ -0,0 +1,9 @@
+function checkStatus(response) {
+  if (response.status >= 200 && response.status < 300) {
+    return response;
+  }
+  const error = new Error(response.statusText);
+  error.response = response;
+  throw error;
+}
+export default checkStatus;
--- day04/03-react-redux-webpack-hmr/app/api/utilities/request.js
+++ day04/03-react-redux-webpack-hmr/app/api/utilities/request.js
@@ -1,6 +1,9 @@
-const request = (url, options={}) =>
-    fetch(url, options)
-        .then(response => response.json())
-        .catch(error => throw new Error(error));
-        
+import checkStatus from './checkStatus';
+
+const request = (url, options = {}) =>
+  fetch(url, options)
+    .then(checkStatus)
+    .then(response => response.json())
+    .catch((error) => { throw new Error(error); });
+
 export default request;
